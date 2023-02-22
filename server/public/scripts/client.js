b40315db1027e9050487847e3e62edd425c79d43
julie gonzalez-kincaid <juliegonzalezkincaid@gmail.com>
2023-02-22 16:55:21 -0500

    tweaking

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,19 @@
 console.log('script sourced.');
+
+
+console.log('JS Sourced');
+
+function getTrains() {
+    axios.get('/train').then((response) => {
+        console.log(response);
+        let trainFromServer = response.data;
+        let contentDiv = document.querySelector('#content');
+        for (let train of trainFromServer) {
+            contentDiv.innerHTML += `
+                <p>${train.name}, ${train.color}</p>
+            `;
+        }
+    });
+}
+
+getTrains();
